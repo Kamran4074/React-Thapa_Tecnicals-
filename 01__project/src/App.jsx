3124514026204f348d79a1da02548f41c210ff21
kamran alam <kammykamran0093@gmail.com>
2025-10-27 02:05:11 +0530

    making a Card

---
+++ 01__project/src/App.jsx
@@ -0,0 +1,15 @@
+export const App=()=>{
+  return (
+    <div>
+      <div>
+        <img src="Khakee.webp" alt="img" height="40%" width="40%" />
+      </div>
+        <h2>Name: Khakee</h2>
+        <h3>Rating:8.2</h3>
+        <p>Summary: Khakee is a 2004 Bollywood action thriller about a team of police officers 
+          escorting a falsely accused "terrorist" across India. The routine mission turns deadly as they are relentlessly
+          pursued by a ruthless former cop, uncovering a shocking conspiracy that reaches the highest levels of power. 
+        </p>
+    </div>
+  )
+}
